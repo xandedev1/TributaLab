ac59ce34d2fe06a0b7ac35ab7728b5814ab666d5
AlessandroChiareleDev <dailyrespostaia@gmail.com>
2026-08-06 10:55:32 -0300

    fix: root route to fiscal_auditor dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "dashboard#index"
+  root "fiscal_auditor/dashboard#show"
 
   get "legal_basis", to: "legal_basis#index", as: :legal_basis
 
