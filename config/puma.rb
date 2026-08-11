40b0db81c5316b0638eca53879a3a8e7c1f5a23a
AlessandroChiareleDev <dailyrespostaia@gmail.com>
2026-08-11 12:37:25 -0300

    fix: puma bind to 0.0.0.0 and solid_queue check

--- config/puma.rb
+++ config/puma.rb
@@ -29,13 +29,13 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments.
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] == "true"
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
